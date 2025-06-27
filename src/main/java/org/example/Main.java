e7a50015dac8045b695fab48b2f028d147de3931
EmanMohamedAli <eman.mohamed@paysky.io>
2025-06-27 18:51:36 +0300

    Task1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,8 +16,7 @@ public class Main {
         driver.findElement(By.cssSelector(".button-1.register-next-step-button")).click();
         String RegisterSuccess = driver.findElement(By.cssSelector(".result")).getText();
         System.out.println(RegisterSuccess);
-        System.out.println(RegisterSuccess);
-       // driver.findElement(By.linkText("Log out")).click();
+        driver.findElement(By.linkText("Log out")).click();
         driver.findElement(By.xpath("//input[@id='Email']")).sendKeys("EmanAliTest@test.com");
         driver.findElement(By.cssSelector("input[name='Password']")).sendKeys("Emtest123#");
     }
